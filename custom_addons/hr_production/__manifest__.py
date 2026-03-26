2683b98e3ad6c874c1051ccf38ed439d64c379dc
randrianjarisoa <randrianjarisoa@gmail.com>
2026-03-26 12:42:28 +0300

    initialisation du projet structure du module hr_production

---
+++ custom_addons/hr_production/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Gestion du Personnel et Production',
+    'version': '1.0',
+    'category': 'Human Resources',
+    'summary': 'Gestion des présences, production et paie',
+    'depends': ['hr'], # On dépend du module employé standard
+    'data': [
+        # Nous ajouterons les fichiers XML ici plus tard
+    ],
+    'installable': True,
+    'application': True,
+}
